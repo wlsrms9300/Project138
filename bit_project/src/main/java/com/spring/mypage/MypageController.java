62f1b2894c3ff85561b0457ca97952241a1962c4
JG <wlsrms9300@naver.com>
2020-01-10 11:38:50 +0900

    홈컨트롤러삭제

--- bit_project/src/main/java/com/spring/mypage/MypageController.java
+++ bit_project/src/main/java/com/spring/mypage/MypageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController {
+public class MypageController {
 	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
