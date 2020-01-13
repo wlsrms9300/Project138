d3912755b0310a561119fcf99061e3a631f18683
JG <wlsrms9300@naver.com>
2020-01-13 19:24:59 +0900

    123

---
+++ bit_project/bit_project/src/main/java/com/spring/mypage/MypageController.java
@@ -0,0 +1,20 @@
+package com.spring.mypage;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MypageController {
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		
+		
+		return "mypage_main";
+	}
+	
+}
